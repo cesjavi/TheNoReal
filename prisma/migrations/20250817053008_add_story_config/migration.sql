0aec2a0d6e8ad0ae9a2d4b957a86c5fbe64353dd
cesjavi <cesjavi@gmail.com>
2025-08-17 17:13:57 -0300

    feat: switch Prisma to PostgreSQL

--- prisma/migrations/20250817053008_add_story_config/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Story" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "optionsPerDecision" INTEGER NOT NULL,
-    "endingMode" TEXT NOT NULL,
-    "chaptersCount" INTEGER,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
