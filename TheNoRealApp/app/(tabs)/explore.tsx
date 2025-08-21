04409506feb33a41bd32f3ee357c748e6efb264f
cesjavi <cesjavi@gmail.com>
2025-08-21 19:24:20 -0300

    refactor: use theme colors

--- TheNoRealApp/app/(tabs)/explore.tsx
+++ TheNoRealApp/app/(tabs)/explore.tsx
@@ -6,15 +6,16 @@ import ParallaxScrollView from '../../components/ParallaxScrollView';
 import { ThemedText } from '../../components/ThemedText';
 import { ThemedView } from '../../components/ThemedView';
 import { IconSymbol } from '../../components/ui/IconSymbol';
+import { Colors } from '../../constants/Colors';
 
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerBackgroundColor={{ light: Colors.light.exploreHeader, dark: Colors.dark.exploreHeader }}
       headerImage={
         <IconSymbol
           size={310}
-          color="#808080"
+          color={Colors.light.muted}
           name="chevron.left.forwardslash.chevron.right"
           style={styles.headerImage}
         />
@@ -97,7 +98,7 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
+    color: Colors.light.muted,
     bottom: -90,
     left: -35,
     position: 'absolute',
