10555692eda0740a6026108879b7484b28c15a2e
cesjavi <cesjavi@gmail.com>
2025-08-21 13:00:51 -0300

    chore: remove unused imports from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import StoryForm from './components/StoryForm';
-import { useRouter, useSearchParams, usePathname } from 'next/navigation';
-
 
 export default function Home() {
   return <StoryForm />;
