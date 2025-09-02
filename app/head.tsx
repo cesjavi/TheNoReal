4af0c5d2ecf1a0c2426f29897c6e5cad709b2702
cesjavi <cesjavi@gmail.com>
2025-09-01 23:14:23 -0300

    sss

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta name="google-adsense-account" content="ca-pub-9368869720669033" />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9368869720669033"
+        crossOrigin="anonymous"
+      />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -36,14 +36,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <head>
-      <meta name="google-adsense-account" content="ca-pub-9368869720669033"></meta>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9368869720669033"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+    <head>      
       </head>       
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <LanguageProvider>
