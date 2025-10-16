9cba2b3bab881f2f21d05fc68237374bbf929db0
cesjavi <cesjavi@gmail.com>
2025-10-16 14:39:38 -0300

    Mark loading screen as client component

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,12 @@
+'use client';
+
+import { useCallback } from 'react';
 import SplashScreen from './components/SplashScreen';
 
 export default function Loading() {
-  const handleOnFinish = () => {
+  const handleOnFinish = useCallback(() => {
     console.log('Loading finished');
-  };
+  }, []);
   return <SplashScreen onFinish={handleOnFinish} />;
 }
 
