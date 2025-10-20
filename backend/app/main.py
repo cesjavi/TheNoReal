2a65ab520fabb81c74b846fa455e9c8ee7f6c5d1
cesjavi <cesjavi@gmail.com>
2025-10-19 22:14:52 -0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,8 +34,8 @@ else:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
